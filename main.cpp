fdba666964472ca12a38619453333565384e68ae
thewarsawpakt <smallperson07@gmail.com>
2020-10-19 21:10:50 -0500

    Used better language.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,6 @@
 // #include "supplyline/supplyline.cpp"
 int main(){
     Ship myShip = Ship(10, 10, "moe");
-    Ship enemyShip = Ship(10, 15, "lester");
+    Ship enemyShip = Ship(10, 15, "the cooler moe");
     myShip.takeDamage(enemyShip);
 }
